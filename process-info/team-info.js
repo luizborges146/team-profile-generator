4cb54be1d529c26c7e17bef231a41ba8409c4a0d
luizborges146 <113717656+luizborges146@users.noreply.github.com>
2022-10-23 21:58:49 -0700

    Create const variables that will hold the html lierals template information

--- process-info/team-info.js
+++ process-info/team-info.js
@@ -1,3 +1,59 @@
 const generateTeamInfo = (team) => {
     console.log(team);
+
+    //Create an empty array to add html elements
+    const teamArray = [];
+
+    const createManager = manager => {
+
+        let managerArray = `
+        <div class="card" style="width: 18rem;">
+            <div class="card-header">
+           ${manager.name} <br/>
+           <i class="fas fa-mug-hot"></i>Manager</div>
+           <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${manager.id}</li>
+            <li class="list-group-item">Email: <span id="email"><a href="mailto:${manager.email}">${manager.email}</a></span></li>
+            <li class="list-group-item">Office Number: ${manager.officeNumber}</li>
+            </ul>
+        </div>
+        `;
+        
+        teamArray.push(managerArray)
+    };
+    const createEngeneer = engeneer => {
+
+        let engineerArray = `
+        <div class="card" style="width: 18rem;">
+            <div class="card-header">
+           ${engeneer.name} <br/>
+           <i class="fas fa-mug-hot"></i>Manager</div>
+           <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${engeneer.id}</li>
+            <li class="list-group-item">Email: <span id="email"><a href="mailto:${engeneer.email}">${engeneer.email}</a></span></li>
+            <li class="list-group-item">Office Number: ${engeneer.gitUsername}</li>
+            </ul>
+        </div>
+        `;
+        
+        teamArray.push(engineerArray)
+    };
+
+    const createInter = intern => {
+
+        let internArray = `
+        <div class="card" style="width: 18rem;">
+            <div class="card-header">
+           ${intern.name} <br/>
+           <i class="fas fa-mug-hot"></i>Manager</div>
+           <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${intern.id}</li>
+            <li class="list-group-item">Email: <span id="email"><a href="mailto:${intern.email}">${intern.email}</a></span></li>
+            <li class="list-group-item">Office Number: ${intern.school}</li>
+            </ul>
+        </div>
+        `;
+        
+        teamArray.push(internArray)
+    };
 }
