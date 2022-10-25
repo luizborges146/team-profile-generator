39e9c2969de343e450177eb7ef30145abfaf3e37
luizborges146 <113717656+luizborges146@users.noreply.github.com>
2022-10-24 23:28:30 -0700

    fixed an issue related to the GitHub user name showed as undefined and also an issue related to the generate HTML that was overwriting the HTML and removing the CSS style i created

--- process-info/team-info.js
+++ process-info/team-info.js
@@ -4,56 +4,63 @@ const generateTeamInfo = (team) => {
     //Create an empty array to add html elements
     const teamArray = [];
 
+    // ##################  Create a manager  ##################
     const createManager = manager => {
 
-        let managerArray = `
-        <div class="card" style="width: 18rem;">
-            <div class="card-header">
-           ${manager.name} <br/>
-           <i class="fas fa-mug-hot"></i> Manager</div>
-           <ul class="list-group list-group-flush">
-            <li class="list-group-item">ID: ${manager.id}</li>
-            <li class="list-group-item">Email: <span id="email"><a href="mailto:${manager.email}">${manager.email}</a></span></li>
-            <li class="list-group-item">Office Number: ${manager.officeNumber}</li>
-            </ul>
-        </div>
-        `;
+        let managerArray = 
+`
+<div class="card">
+    <div class="card-header">
+        ${manager.name} <br/>
+        <i class="fas fa-mug-hot"></i> Manager</div>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: ${manager.id}</li>
+        <li class="list-group-item">Email: <span id="email"><a href="mailto:${manager.email}">${manager.email}</a></span></li>
+        <li class="list-group-item">Office Number: ${manager.officeNumber}</li>
+    </ul>
+</div>
+`;
         
         teamArray.push(managerArray)
     };
+
+    // ##################  Create a Engineer  ##################
     const createEngineer = engineer => {
 
-        let engineerArray = `
-        <div class="card" style="width: 18rem;">
-            <div class="card-header">
-           ${engineer.name} <br/>
-           <i class="fa-solid fa-code"></i> Engineer</div>
-           <ul class="list-group list-group-flush">
-            <li class="list-group-item">ID: ${engineer.id}</li>
-            <li class="list-group-item">Email: <span id="email"><a href="mailto:${engineer.email}">${engineer.email}</a></span></li>
-            <li class="list-group-item">Github Username: <a target="_blank" href="https://github.com/${engineer.githubUsername}">${engineer.githubUsername}</a></li>
-            </ul>
-        </div>
-        `;
+        let engineerArray =
+`
+<div class="card" style="width: 18rem;">
+    <div class="card-header">
+        ${engineer.name} <br/>
+        <i class="fa-solid fa-code"></i> Engineer</div>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: ${engineer.id}</li>
+        <li class="list-group-item">Email: <span id="email"><a href="mailto:${engineer.email}">${engineer.email}</a></span></li>
+        <li class="list-group-item">Github Username: <a target="_blank" href="https://github.com/${engineer.gitUsername}">${engineer.gitUsername}</a></li>
+    </ul>
+</div>
+`;
         
         teamArray.push(engineerArray)
     };
 
+    // ##################  Create a Intern  ##################
+
     const createIntern = intern => {
 
-        let internArray = `
-        <div class="card" style="width: 18rem;">
-            <div class="card-header">
-           ${intern.name} <br/>
-           <i class="fa-solid fa-user-graduate"></i> Inter</div>
-           
-           <ul class="list-group list-group-flush">
-            <li class="list-group-item">ID: ${intern.id}</li>
-            <li class="list-group-item">Email: <span id="email"><a href="mailto:${intern.email}">${intern.email}</a></span></li>
-            <li class="list-group-item">School: ${intern.school}</li>
-            </ul>
-        </div>
-        `;
+        let internArray = 
+`
+<div class="card" style="width: 18rem;">
+    <div class="card-header">
+        ${intern.name} <br/>
+        <i class="fa-solid fa-user-graduate"></i> Inter</div>         
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: ${intern.id}</li>
+        <li class="list-group-item">Email: <span id="email"><a href="mailto:${intern.email}">${intern.email}</a></span></li>
+        <li class="list-group-item">School: ${intern.school}</li>
+    </ul>
+</div>
+`;
         
         teamArray.push(internArray)
     };
@@ -81,20 +88,21 @@ module.exports = team => {
     <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
         integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <script src="https://kit.fontawesome.com/00f313d45b.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="../dist/style.css" />
-    <title>Team Profile Generator</title>
+<script src="https://kit.fontawesome.com/00f313d45b.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" type="text/css" href="./style.css">
+<title>Team Profile Generator</title>
 </head>
+
 <body>
-    <header>
+<header>
     <h1> My Team </h1>
-    </header>
-    <main> ${generateTeamInfo(team)} </main>
+</header>
+<main> ${generateTeamInfo(team)} </main>
      
 </body>
 </html>
